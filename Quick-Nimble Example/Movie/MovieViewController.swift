6d2a47ef878dae82fa735f9a7b4d2949c376b154
Fomagran <fomagran6@naver.com>
2021-08-30 18:45:42 +0900

    [add] Movie테이블뷰에 데이터 세팅

--- Quick-Nimble Example/Movie/MovieViewController.swift
+++ Quick-Nimble Example/Movie/MovieViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class MovieViewController: UIViewController {
 
+    //MARK:- IBOutlets
+    
     @IBOutlet weak var table: UITableView!
+    
+    //MARK:- Properties
+    
+    var movies:[Movie] {
+        return MoviesDataHelper.getMovies()
+    }
+    
+    //MARK:- Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+       configure()
+    }
+    
+    //MARK:- Helper Functions
+    
+    private func configure() {
+        table.dataSource = self
+    }
+}
+
+//MARK:- UITableViewDataSource
+
+extension MovieViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "MovieTableViewCell")
+        cell?.textLabel?.text = movies[indexPath.row].title
+        cell?.detailTextLabel?.text = movies[indexPath.row].genreString()
+        return cell!
     }
 }
 
